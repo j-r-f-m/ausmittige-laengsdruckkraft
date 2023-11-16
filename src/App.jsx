fc35bd07af22eb28047e29fc4cefe3a1cb68f104
jrfm <moesch.jonas@protonmail.com>
2023-11-16 15:20:55 +0100

    Add basic project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
+import { MathJaxContext } from "better-react-mathjax";
+import AusmittigeLaengsdruckKraft from "./page/ausmittige-längsdruckkraft/AusmittigeLaengsdruckKraft";
+
+const config = {
+  "fast-preview": {
+    disabled: true,
+  },
+  tex2jax: {
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"],
+    ],
+    displayMath: [
+      ["$$", "$$"],
+      ["\\[", "\\]"],
+    ],
+  },
+  messageStyle: "none",
+};
+
 function App() {
   return (
-    <>
-      <h1>Hi Ho</h1>
-    </>
+    <MathJaxContext
+      version={2}
+      config={config}
+      onStartup={(mathJax) => (mathJax.Hub.processSectionDelay = 0)}
+    >
+      <AusmittigeLaengsdruckKraft />
+    </MathJaxContext>
   );
 }
 
---
+++ src/page/ausmittige-längsdruckkraft/AusmittigeLaengsdruckKraft.jsx
@@ -0,0 +1,12 @@
+import CssBaseline from "@mui/material/CssBaseline";
+
+function AusmittigeLaengsdruckKraft() {
+  return (
+    <>
+      <CssBaseline />
+      <h1>Hi HO</h1>
+    </>
+  );
+}
+
+export default AusmittigeLaengsdruckKraft;
